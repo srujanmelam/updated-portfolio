e0b0a6cc5a122fabbf80b5698c75ba6e045f9468
Jai bala srujan Melam <jaibalasrujanmelam@Jais-MacBook-Air.local>
2025-02-24 04:41:46 +0530

    made changes

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,20 +1,27 @@
 export const experienceData = [
     {
         id: 1,
+        company: 'Turvo Inc',
+        jobtitle: 'Associate Software Engineer',
+        startYear: 'March 2024',
+        endYear: 'Present'
+    },
+    {
+        id: 2,
         company: 'NextEducation Pvt Ltd',
         jobtitle: 'Software Engineer I',
         startYear: 'Nov 2023',
         endYear: 'Present'
     },
     {
-        id: 2,
+        id: 3,
         company: 'AutomationEdge Technologies',
         jobtitle: 'Development Intern',
         startYear: 'Jun 2022',
         endYear: 'Jun 2023'
     },
     {
-        id: 3,
+        id: 4,
         company: 'Smart Knower Pvt Ltd',
         jobtitle: 'Machine Learning Intern',
         startYear: 'Feb 2022',
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     desciption: "Hello! I am MELAM JAI BALA SRUJAN, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
     image: 'https://i.ibb.co/VHxCVfH/profile.jpg',
     imagebw: 'https://i.ibb.co/VHxCVfH/profile.jpg',
-    resumePdf: 'https://aqua-lucia-90.tiiny.site/'
+    resumePdf: 'https://link-new-resume.tiiny.site'
 }
